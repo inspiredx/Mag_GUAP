4f6779b1ad30041d5e74a5fd3f94e4799ed32e5f
inspiredx <georgiykapustinx@gmail.com>
2022-02-11 21:14:22 +0300

    Day 2

---
+++ script.js
@@ -0,0 +1,24 @@
+$(document).ready(function ($) {
+    $('.popup-open').click(function () {
+        $('.popup-fade').fadeIn();
+        return false;
+    });
+
+    $('.popup-close').click(function () {
+        $(this).parents('.popup-fade').fadeOut();
+        return false;
+    });
+
+    $(document).keydown(function (e) {
+        if (e.keyCode === 27) {
+            e.stopPropagation();
+            $('.popup-fade').fadeOut();
+        }
+    });
+
+    $('.popup-fade').click(function (e) {
+        if ($(e.target).closest('.popup').length == 0) {
+            $(this).fadeOut();
+        }
+    });
+});
